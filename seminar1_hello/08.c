83d0ed6290f00c9a421e14a2dc3c3728043b2ad0
Fedosik_Kokosik <bykovfedor2@gmail.com>
2026-09-23 11:15:55 +0300

    Update 08.c

--- seminar1_hello/08.c
+++ seminar1_hello/08.c
@@ -4,13 +4,11 @@ int main(void) {
     int a, b, c;
     scanf("%d %d %d", &a, &b, &c);
 
-    // Находим первое число, кратное c, которое >= a
     int start = a;
     if (start % c != 0) {
         start = start + (c - (start % c));
     }
 
-    // Печатаем числа с шагом c, используя флаг для пробелов
     int first = 1;
     for (int i = start; i <= b; i += c) {
         if (!first) {
